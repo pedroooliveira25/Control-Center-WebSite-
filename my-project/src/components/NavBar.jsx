f205acbf51e7353c20cb108ee699ef4d35c8765b
pedroooliveira25 <eu.pedrohfo@gmail.com>
2025-11-01 12:16:04 -0300

    Corringdo cores do  NavBar

--- my-project/src/components/NavBar.jsx
+++ my-project/src/components/NavBar.jsx
@@ -1,13 +1,12 @@
 function NavBar() {
     return(
-        <nav className="fixed top-0 w-full bg-linear-to-r from-blue-800 to to-blue-400 **:backdrop-blur-md z-50">
+        <nav className="fixed top-0 w-full bg-linear-to-r from-blue-910 to-blue-500 to-white-800 **:backdrop-blur-md z-50">
             <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-center gap-8"> 
-              
               <a href="#informacoes"className="hover:text-gray-200 text-white text-sm">Sobre</a>
               <a href="#quemsomos"className="hover:text-gray-200 text-white text-sm">Quem somos</a>
               <a href="#cases"className="hover:text-gray-200 text-white text-sm">Cases de sucesso</a>
               <a className="hover:text-gray-200 text-white text-sm">Suporte</a>
-              
+
             </div>
         </nav>
     )
