8469a5bc0257981578b68de25aebb40e0ea164f4
pedroooliveira25 <eu.pedrohfo@gmail.com>
2025-11-01 12:02:50 -0300

    add NavBar

---
+++ my-project/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+    return(
+        <nav className="fixed top-0 w-full bg-linear-to-r from-blue-800 to to-blue-400 **:backdrop-blur-md z-50">
+            <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-center gap-8"> 
+              
+              <a href="#informacoes"className="hover:text-gray-200 text-white text-sm">Sobre</a>
+              <a href="#quemsomos"className="hover:text-gray-200 text-white text-sm">Quem somos</a>
+              <a href="#cases"className="hover:text-gray-200 text-white text-sm">Cases de sucesso</a>
+              <a className="hover:text-gray-200 text-white text-sm">Suporte</a>
+              
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
